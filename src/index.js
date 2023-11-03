63b303af25d37581cd390f62048ba967ee07ecbc
nicksolony <nicksolony@gmail.com>
2023-11-03 14:00:30 -0400

    restored previous home work

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/goit-react-hw-08-phonebook">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
