59537e4158f27aab1546fdc856cec4415edd5603
nicksolony <nicksolony@gmail.com>
2023-11-03 20:07:48 -0400

    Added home screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
 import { Container } from './Container/Container';
 import { Navigation } from './Navigation/Navigation';
 import { Routes, Route } from 'react-router-dom';
 import { Register }  from 'pages/register/Register';
 import { Login } from 'pages/login/Login';
 import { Contacts } from 'pages/contacts/Contacts';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { authOperations } from 'redux/auth';
+import {Home} from 'pages/home/Home'
 
 
 export const App = () => {
 
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
 
     
     return (
       <Container>
         <Navigation />
         <Routes>
+          <Route exact path='/' element={<Home/>}/>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/contacts" element={<Contacts/>}/>
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,5 @@
+export const Home = () => {
+    return (
+        <h1>Welcome to contacts book!</h1>
+    )
+}
